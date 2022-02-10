2c9b49db82a9e80a553fb2e31c432ba4802ec26e
Sayemm <sayemseer@gmail.com>
2022-02-10 18:03:36 +0600

    bacground added for log in screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.new_layout)
+        setContentView(R.layout.back_ground)
     }
 
 
