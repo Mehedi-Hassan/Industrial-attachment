60b7a7fc4c71b901681f449a473b4cefa54b8b02
sumaiya <sumaiya.sultana.710@gmail.com>
2022-02-14 03:51:26 +0600

    home navigation onclick incomplete

--- app/src/main/java/com/example/project1/HomeActivity.kt
+++ app/src/main/java/com/example/project1/HomeActivity.kt
@@ -1,11 +1,36 @@
 package com.example.project1
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
+
+        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.icon_color -> {
+                    val intent = Intent(this, RegisterActivity::class.java)
+                    startActivity(intent)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.icon_user -> {
+                    val intent = Intent(this, RegisterActivity::class.java)
+                    startActivity(intent)
+                    return@OnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+        }
+        val menuBar: FrameLayout = findViewById(R.id.menuBar)
+
+        //menuBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
